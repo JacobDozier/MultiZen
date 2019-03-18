c46f0676a21482e1812737abb5344a5bfd1507bc
JacobDozier <jacobadozier@gmail.com>
2019-03-18 16:53:52 -0500

    Tried turning the dropdowns into templates. Imported Handlebars.

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 $(function() {
     var client = ZAFClient.init();
     client.invoke('resize', { width: '100%', height: '410px' });
+    showInfo();
 });
+
+function showInfo() {
+    var project_data = {
+        'value': 'MultiZen',
+        'value1': 'value1',
+        'value2': 'value2',
+        'value3': 'value3'
+    };
+
+    var source = $("#time-template").html();
+    var template = Handlebars.compile(source);
+    var html = template(project_data);
+    $("#content").html(html);
+}
