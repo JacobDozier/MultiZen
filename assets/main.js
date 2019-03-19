021dc7853abe081311261c694314f21c8b0c4df4
JacobDozier <jacobadozier@gmail.com>
2019-03-18 21:20:18 -0500

    Attempted to get a Handlebar loop to display a list. Still needs work.

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,20 @@
 $(function() {
     var client = ZAFClient.init();
-    client.invoke('resize', { width: '100%', height: '410px' });
+    client.invoke('resize', { width: '100%', height: '350px' });
     showInfo();
 });
 
+// Insert data obtained from the Harvest api.
 function showInfo() {
-    var project_data = {
+    var project_data = 
+    // ["MultiZen", "Value1", "Value2", "Value3"];
+    {
         'value': 'MultiZen',
         'value1': 'value1',
         'value2': 'value2',
         'value3': 'value3'
     };
+    console.log(project_data);
 
     var source = $("#time-template").html();
     var template = Handlebars.compile(source);
