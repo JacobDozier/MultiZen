9668b3b68acf3b4a1c79d19c03ab633afd60531e
JacobDozier <jacobadozier@gmail.com>
2019-03-28 16:52:07 -0500

    Changed the api call, accidentally included node commands which didn't work.

--- assets/main.js
+++ assets/main.js
@@ -11,15 +11,15 @@ function showProject() {
     var source = $("#project-template").html();
     var template = Handlebars.compile(source);
 
-    var project_data = 
-    {
-        test: [
-            {header:"ACBJ", categories: ["Billable", "Support", "Non-Support"]},
-            {header:"ALM", categories: ["Billable", "Support", "Non-Support"]},
-            {header: "IBJ", categories: ["Billable", "Support", "Non-Support"]},
-            {header: "DSI", categories: ["Internal Time"]}
-        ]
-    }
+    var project_data = callHarvest("v2/projects");
+    // {
+    //     test: [
+    //         {header:"ACBJ", categories: ["Billable", "Support", "Non-Support"]},
+    //         {header:"ALM", categories: ["Billable", "Support", "Non-Support"]},
+    //         {header: "IBJ", categories: ["Billable", "Support", "Non-Support"]},
+    //         {header: "DSI", categories: ["Internal Time"]}
+    //     ]
+    // }
     
     $("#content").html(template(project_data));
 }
@@ -29,10 +29,18 @@ function showTask() {
     var source = $("#task-template").html();
     var template = Handlebars.compile(source);
 
+    task_data = callHarvest("/v2/tasks");
+    
+    $("#content").html(template(task_data));
+}
+
+function callHarvest(apiPath) {
+    // const https = require("https");
+
     const options = {
         protocol: "https:",
         hostname: "api.harvestapp.com",
-        path: "v2/tasks",
+        path: apiPath,
         headers: {
             "User-Agent": "MultiZen Testing",
             "Authorization": "Bearer 1798431.pt.nVpdLY3VMBZ-Z2-K2GBwL5H_ObIJHFpCWo82YSfDWcf7eQJ3q_bKyGkN29uuddg6sRR6Cch3_yCcPOEMv3MtBg",
@@ -40,28 +48,38 @@ function showTask() {
         }
     }
 
-    https.get(options, (res) => {
-        const { statusCode } = res;
-      
-        if (statusCode !== 200) {
-          console.error(`Request failed with status: ${statusCode}`);
-          return;
+    $.ajax({
+        url: "https://api.harvestapp.com/" + apiPath,
+        headers: {
+            "User-Agent": "MultiZen Testing",
+            "Authorization": "Bearer 1798431.pt.nVpdLY3VMBZ-Z2-K2GBwL5H_ObIJHFpCWo82YSfDWcf7eQJ3q_bKyGkN29uuddg6sRR6Cch3_yCcPOEMv3MtBg",
+            "Harvest-Account-ID": "1009919"
         }
+    }).then(function(data) {
+       $('.greeting-id').append(data.id);
+       $('.greeting-content').append(data.content);
+    });
+
+    // https.get(options, (res) => {
+    //     const { statusCode } = res;
       
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-          try {
-            const parsedData = JSON.parse(rawData);
-            console.log(parsedData);
-          } catch (e) {
-            console.error(e.message);
-          }
-        });
-      }).on('error', (e) => {
-        console.error(`Got error: ${e.message}`);
-      });
-    
-    $("#content").html(template(project_data));
+    //     if (statusCode !== 200) {
+    //       console.error(`Request failed with status: ${statusCode}`);
+    //       return;
+    //     }
+      
+    //     res.setEncoding('utf8');
+    //     let rawData = '';
+    //     res.on('data', (chunk) => { rawData += chunk; });
+    //     res.on('end', () => {
+    //       try {
+    //         const parsedData = JSON.parse(rawData);
+    //         console.log(parsedData);
+    //       } catch (e) {
+    //         console.error(e.message);
+    //       }
+    //     });
+    //   }).on('error', (e) => {
+    //     console.error(`Got error: ${e.message}`);
+    // });
 }
